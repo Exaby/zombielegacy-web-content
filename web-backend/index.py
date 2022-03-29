ea640de0b69a4519235fa72a99c969725ab3efff
Spacey_Core <59103833+Exaby@users.noreply.github.com>
2022-03-29 00:56:37 -0400

    fixed json output

--- web-backend/index.py
+++ web-backend/index.py
@@ -18,8 +18,8 @@ class webDeploy(BaseHTTPRequestHandler):
         maxplayers = (a2s.info(serveraddress).max_players)
         playercount = (a2s.info(serveraddress).player_count)
         servername = (a2s.info(serveraddress).server_name)
-        jsonout = ('"ls_zs": {\n\t"ip": "' + str(address) + ':' + str(port) + '",\n\t"Map": "' + str(mapname) + '",\n\t"MaxPLayers": ' +
-        str(maxplayers) + ',\n\t"PLayerCount": ' + str(playercount) + ',\n\t"ServerName": "' + str(servername) + '",\n},')
+        jsonout = ('{\n"ls_zs": {\n\t"ip": "' + str(address) + ':' + str(port) + '",\n\t"Map": "' + str(mapname) + '",\n\t"MaxPLayers": ' +
+        str(maxplayers) + ',\n\t"PLayerCount": ' + str(playercount) + ',\n\t"ServerName": "' + str(servername) + '",\n},\n}')
         self.wfile.write(bytes(jsonout, "utf-8"))
 
 
