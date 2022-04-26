1f9faf1b8c191d9ce674ea989d43545ad793ffdd
Spacey_Core <59103833+Exaby@users.noreply.github.com>
2022-04-26 09:51:32 -0400

    same here

--- portal/auto-refresh.js
+++ portal/auto-refresh.js
@@ -1,7 +1,7 @@
 var previous = null;
 var current = null;
 setInterval(function() {
-    $.getJSON("http://127.0.0.1:5500/servers.json", function(json) {
+    $.getJSON("https://zlzs.spco.xyz/api/servers.json", function(json) {
         current = JSON.stringify(json);            
         if (previous && current && previous !== current) {
             console.log('refresh');
